00c14885b169ea48e1248a4ce601a313da00531a
Dylan Caballero <dylancaballero@Dylans-iMac.local>
2020-10-22 19:06:56 -0400

    Deploying

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.use("/agents/:id/reviews", reviewRoutes);
 
 
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, function(){ 
     console.log("Serving on port" + port);
 });
--- routes/reviews.js
+++ routes/reviews.js
@@ -5,19 +5,6 @@ var Review = require("../models/review");
 var middleware = require("../middleware/index.js");
 
 
-//INDEX
-router.get("/", function(req, res){
-    User.findById(req.params.id).populate({
-        path: "reviews",
-        options: {sort: {createdAt:-1}}// sorting the populated reviews array to show the latest first
-    }).exec(function(err, agent){
-        if(err || !agent) {
-            req.flash("error", err.message);
-            return res.redirect("back");
-        }
-        res.render("reviews/index", {agent: agent});
-    });
-});
 
 //NEW 
 router.get("/new", middleware.isLoggedIn, function(req, res){
